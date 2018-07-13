5561e696ce2dc1acf0a10dfd023714bc26fb6e4d
shawndoyle <34788350+shawndoyle@users.noreply.github.com>
2018-07-13 19:34:16 +0800

    Finished scraper function, website has mistakes in html.  Need to make a separate file to fix those.

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -16,77 +16,52 @@ const pg = require('knex')({
 });
 
 //Test cases
-const onePlay = ['allswell']
-const threePlays = ['macbeth', 'allswell', 'troilus_cressida']
 const playNames = ['allswell', 'asyoulikeit', 'comedy_errors', 'cymbeline', 'lll', 'measure', 'merry_wives', 'merchant', 'midsummer', 'much_ado', 'pericles', 'taming_shrew', 'tempest', 'troilus_cressida', 'twelfth_night', 'two_gentlemen', 'winters_tale', '1henryiv', '2henryiv', 'henryv', '1henryvi', '2henryvi', '3henryvi', 'henryviii', 'john', 'richardii', 'richardiii', 'cleopatra', 'coriolanus', 'hamlet', 'julius_caesar', 'lear', 'macbeth', 'othello', 'romeo_juliet', 'timon', 'titus']
 
 
 
 //Main function
+Promise.all(playNames.map(addPlayToDatabase)).then(finish)
 
-threePlays.forEach((title) => {
-	addPlayToDatabase(title);	
-})
-
-
+function finish() {
+	console.log("\n\tData scrape complete!\n")
+	process.exit(0)
+}
 
 async function addPlayToDatabase(title) {
-	//Check for play in public.plays
-	let play_id = await pg('plays').select('id').where({name: title})
-	if(await play_id.length === 0) {
-		console.log(`${title}: Adding to public.plays`)
-		try {
+	try{
+		//Check for play in public.plays
+		let play_id = await pg('plays').select('id').where({name: title})
+		if(await play_id.length === 0) {
+			console.log(`${title}: Adding to public.plays`)
 			play_id = (await pg('plays').insert({name: title}, 'id'))[0]
-		} catch (reason) {
-			console.error(reason)
-			fs.appendFile('error.log', "---Insert Play Error---\n" 
-				+ `title: ${title}\n` 
-				+ reason + "\n\n", (err) => {
-				if(err) {
-					console.error("ERROR:  Unable to append to error.log")
-				}
-			})
+		} else {
+			play_id = (await play_id)[0].id
 		}
-	} else {
-		play_id = (await play_id)[0].id
-	}
+		let play_text = await scrapeData(title)
 
-	let play_text = await scrapeData(title) 
-	// let play_text = [
-	// 	Line(4,6,8,"Brack", "Missus Tesman ;)"),
-	// 	Line(4,6,9,"Brack", "People don't do that!"),
-	// ]
-	console.log(`${title}: Adding lines to public.text...`)
-	for(let i = 0; i < await play_text.length; i++){
-		await addLineToTextTable(title, play_id, (await play_text)[i])
+		console.log(`${title}: Adding lines to public.text...`)
+		for(let i = 0; i < await play_text.length; i++){
+			await addLineToTextTable(title, play_id, (await play_text)[i])
+		}
+	} catch (reason) {
+		logError(reason)
 	}
 }
 
 //Adds a line to public.text
 async function addLineToTextTable(title, play_id, nextLine){
-	//Checks for character in public.characters
-	let character_id = await pg('characters').select('id').where({name: nextLine.character, play_id})
-	if(await character_id.length === 0) {
-		console.log(`${title}: Adding character ${nextLine.character} to public.characters`)
-		try {
-			character_id = (await pg('characters').insert({name: nextLine.character, play_id: play_id}, 'id'))[0]
-		} catch (reason) {
-			console.error(reason)
-			fs.appendFile('error.log', "---Insert Character Error---\n" 
-				+ `character: ${nextLine.character}`
-				+ `play_id: ${play_id}` 
-				+ reason + "\n\n", (err) => {
-				if(err) {
-					console.error("ERROR:  Unable to append to error.log")
-				}
-			})
-		}
-	} else {
-		character_id = (await character_id)[0].id
-	}
-
-	//Adds the line
 	try {
+		//Checks for character in public.characters
+		let character_id = await pg('characters').select('id').where({name: nextLine.character, play_id})
+		if(await character_id.length === 0) {
+			console.log(`${title}: Adding character ${nextLine.character} to public.characters`)
+			character_id = (await pg('characters').insert({name: nextLine.character, play_id: play_id}, 'id'))[0]
+		} else {
+			character_id = (await character_id)[0].id
+		}
+
+		//Adds the line
 		const { act, scene, line_no, line } = nextLine
 		await pg('text').insert({
 			play_id,
@@ -97,19 +72,7 @@ async function addLineToTextTable(title, play_id, nextLine){
 			line
 		})
 	} catch (reason) {
-		console.error(reason)
-		fs.appendFile('error.log', "---Insert Line Error---\n"
-			+ `play_id: ${play_id}\n`
-			+ `act: ${act}\n`
-			+ `scene: ${scene}\n`
-			+ `line_no: ${line_no}\n`
-			+ `character_id: ${character_id}\n`
-			+ `line: ${line}`
-			+ reason + "\n\n", (err) => {
-				if(err) {
-					console.error("ERROR:  Unable to append to error.log")
-				}
-			})
+		logError(reason)
 	}
 }
 
@@ -151,7 +114,26 @@ async function scrapeData(title) {
 			}
 		})
 		return await text
-	} catch (e) {
-		console.error(e)
+	} catch (reason) {
+		logError(reason)
+	}
+}
+
+function logError(reason, errorType, variables) {
+	console.error(reason)
+	if(errorType) {
+		const now = new Date().toString()
+		let log = `${now}:  ${errorType}\n\nVariables:\n`
+		if(variables) {
+			Object.keys(variables).forEach((key) => {
+				log += `${key}:  ${variables[key]}\n`
+			})
+		}
+		log += "\n" + reason + "\n\n\n"
+		fs.appendFile('error.log', log, (err)=> {
+			if(err) {
+				console.error(err)
+			}
+		})
 	}
 }
